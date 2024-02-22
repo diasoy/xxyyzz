8444de3c499ea05bf7b94174a131019fba217356
Dias Norman <22082010134@student.upnjatim.ac.id>
2024-02-22 23:59:42 +0700

    [ADD] Project Front End

--- src/components/Project/ProjectHeader.jsx
+++ src/components/Project/ProjectHeader.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import kiddyLearn from "../../assets/UIUX-KiddyLearn Adventures/logo.png";
 import qris from "../../assets/UIUX-QRIS/Logo.png";
+import shopKuy from "../../assets/Web/ShopKuy.png";
 
 const ProjectHeader = () => {
   return (
@@ -22,7 +23,7 @@ const ProjectHeader = () => {
                   to={"/project-experience/kiddy-learn-adventures"}
                   className=""
                 >
-                  <div className="border rounded-xl hover:border-[4px] hover:border-green-700 dark:hover:border-[4px] dark:hover:border-white bg-green-700 text-white transition-all duration-500">
+                  <div className="border-[3px] rounded-xl hover:border-[4px] hover:border-green-700 dark:hover:border-[4px] dark:hover:border-white bg-green-700 text-white transition-all duration-500">
                     <img
                       src={kiddyLearn}
                       alt="kiddylearn"
@@ -41,7 +42,7 @@ const ProjectHeader = () => {
               </li>
               <li>
                 <Link to={"/project-experience/qris"} className="">
-                  <div className="border rounded-xl hover:border-[4px] hover:border-green-700 dark:hover:border-[4px] dark:hover:border-white bg-green-700 text-white transition-all duration-500">
+                  <div className="border-[3px] rounded-xl hover:border-[4px] hover:border-green-700 dark:hover:border-[4px] dark:hover:border-white bg-green-700 text-white transition-all duration-500">
                     <img
                       src={qris}
                       alt="qris"
@@ -53,6 +54,38 @@ const ProjectHeader = () => {
                       </h1>
                       <p className="text-center">
                         Aplikasi Pembayaran dan Penyimpanan Uang Digital
+                      </p>
+                    </div>
+                  </div>
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="flex flex-col justify-center items-center mx-12 md:mx-24 xl:mx-32 mt-8">
+          <div className="flex flex-col mx-2 md:mx-4 lg:mx-6 xl:mx-10 2xl:mx-20">
+            <h1 className="font-semibold text-lg mb-4 underline underline-offset-8">
+              Front End
+            </h1>
+            <ul className="grid gap-6 md:grid-cols-2 xl:grid-cols-3 lg:items-center 2xl:grid-cols-4">
+              <li className="">
+                <Link
+                  to="https://dias-shopkuy-site.vercel.app/"
+                  target="_blank"
+                  className=""
+                >
+                  <div className="border-[3px] rounded-xl hover:border-[4px] hover:border-green-700 dark:hover:border-[4px] dark:hover:border-white bg-green-700 text-white transition-all duration-500">
+                    <img
+                      src={shopKuy}
+                      alt="shopKuy"
+                      className="w-full h-64 rounded-t-lg object-cover"
+                    />
+                    <div className="flex flex-col justify-center items-center py-6 px-3 mx-3 gap-4">
+                      <h1 className="font-semibold 2xl:text-lg text-center">
+                        ShopKuy
+                      </h1>
+                      <p className="text-center">
+                        Landing Page Toko Online Platzi Fake Api Store
                       </p>
                     </div>
                   </div>
